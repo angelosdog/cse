2e70ba045602b0594c67abd1b27b75c9bf404622
vincentt143 <tran.vincent17@gmail.com>
2013-08-12 21:31:16 +1000

    Fucking regex

--- cs2041/lab03/courses.sh
+++ cs2041/lab03/courses.sh
@@ -4,13 +4,13 @@ IFS='
 
 n=0
 
-if [[ $1 != "[A-Z]{4}" ]];
+if [[ $1 = "[A-Z]{4}" ]];
 then
     echo "Usage: $0 argument 1 must be a four capital letter sequence" 1>&2
     exit 1
 fi
 
-for course in `wget -q -O- "http://www.handbook.unsw.edu.au/vbook2013/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=$1"| grep "<TD class" | grep $1 | cut -d "<" -f2,3 | cut -d \" -f5 | cut -d "<" -f1 | cut -d ">" -f2`;
+for course in `wget -q -O- "http://www.handbook.unsw.edu.au/vbook2012/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=$1"| grep "<TD class" | grep $1 | cut -d "<" -f2,3 | cut -d \" -f5 | cut -d "<" -f1 | cut -d ">" -f2`;
 do
     if [ $n = 0 ];
     then
@@ -22,7 +22,7 @@ do
     fi
 done
 
-for course in `wget -q -O- "http://www.handbook.unsw.edu.au/vbook2013/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr=$1"| grep "<TD class" | grep $1 | cut -d "<" -f2,3 | cut -d \" -f5 | cut -d "<" -f1 | cut -d ">" -f2`;
+for course in `wget -q -O- "http://www.handbook.unsw.edu.au/vbook2012/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr=$1"| grep "<TD class" | grep $1 | cut -d "<" -f2,3 | cut -d \" -f5 | cut -d "<" -f1 | cut -d ">" -f2`;
 do
     if [ $n = 0 ];
     then
