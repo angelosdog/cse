dcfdfb65c9e2cd5c91dca89532e9c7be2b436483
Vincent Tran <tran.vincent17@gmail.com>
2013-08-26 20:13:19 +1000

    something

--- cs2041/lab03/courses.sh
+++ cs2041/lab03/courses.sh
@@ -4,12 +4,6 @@ IFS='
 
 n=0
 
-if [[ $1 = "[A-Z]{4}" ]];
-then
-    echo "Usage: $0 argument 1 must be a four capital letter sequence" 1>&2
-    exit 1
-fi
-
 for course in `wget -q -O- "http://www.handbook.unsw.edu.au/vbook2012/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=$1"| grep "<TD class" | grep $1 | cut -d "<" -f2,3 | cut -d \" -f5 | cut -d "<" -f1 | cut -d ">" -f2`;
 do
     if [ $n = 0 ];
---
+++ cs2041/lab05/echon.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+
+if ($#ARGV+1 < 2) {
+    print STDERR "Usage $0\n";
+} elsif ($#ARGV+1 > 2) {
+    print STDERR "Usage $0\n";
+} else {
+    if($1 /\D/
+}
