595b0c378e48cff9cc8661765f7304faf808cfac
vincentt143 <tran.vincent17@gmail.com>
2013-08-27 15:53:51 +1000

    sdfsdf

--- cs2041/lab05/echon.pl
+++ cs2041/lab05/echon.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/perl -w
 
 if ($#ARGV+1 < 2) {
-    print STDERR "Usage $0\n";
+    print STDERR "Usage: $0 Not enough arguments\n";
 } elsif ($#ARGV+1 > 2) {
-    print STDERR "Usage $0\n";
+    print STDERR "Usage: $0 Too many arguments\n";
 } else {
-    if($1 /\D/
+    if($ARGV[0] =~ /^\d+$/) {
+        foreach (1..$ARGV[0]) {
+            print "$ARGV[1]\n";
+        }
+    } else {
+        print STDERR "Usage: $0\n First argument should be a positive integer";
+    }
 }
---
+++ cs2041/lab05/tail.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl -w
+
+if (@ARGV == 0) {
+    print <STDIN>;
+}
